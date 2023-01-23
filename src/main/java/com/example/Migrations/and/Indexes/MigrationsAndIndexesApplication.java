468b920b282024aab146cecc7d0e80ea97865f6e
Pronyushkin_qa <kollaydereva@gmail.com>
2023-01-23 15:06:42 +0300

    Initial commit

---
+++ src/main/java/com/example/Migrations/and/Indexes/MigrationsAndIndexesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Migrations.and.Indexes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationsAndIndexesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationsAndIndexesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Migrations/and/Indexes/MigrationsAndIndexesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Migrations.and.Indexes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationsAndIndexesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
